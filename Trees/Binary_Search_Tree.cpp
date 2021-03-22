9c4c490444d1c4fadf69077f57f5a2c4bffbf4c6
Samarpitdua <samarpit.smarty@gmail.com>
2021-03-23 00:18:57 +0530

    practical
    new practicals

--- Trees/Binary_Search_Tree.cpp
+++ Trees/Binary_Search_Tree.cpp
@@ -1,66 +1,191 @@
-#include <bits/stdc++.h>
-using namespace std;
-class tree
-{
-    public:
-    int data;
-    tree *left, *right;
-    tree();
-   tree(int);
-    tree *insert(tree *, int);
-    void inorder(tree *);
-};
- tree::tree(int value)
-{
-    data = value;
-    left = right = NULL;
-}
-
-tree * tree::insert(tree *root, int value)
-{
-    if (!root)
-        return new tree(value);
-
-    if (value > root->data)
-        root->right = insert(root->right, value);
-    else
-    {
-        root->left = insert(root->left, value);
-    }
-    return root;
-}
- void tree::inorder(tree *root)
-{
-    cout<<"\n";
-    if (root == 0)
-        return;
-    else
-    {
-
-        inorder(root->left);
-        cout << root->data << " ";
-        inorder(root->right);
-    }
-}
-// void deletion()
+// #include <bits/stdc++.h>
+// using namespace std;
+// class tree
 // {
+//     public:
+//     int data;
+//     tree *left, *right;
+//     tree();
+//    tree(int);
+//     tree *insert(tree *, int);
+//     void inorder(tree *);
+// };
+//  tree::tree(int value)
+// {
+//     data = value;
+//     left = right = NULL;
 // }
-int main()
-{
 
-    tree t;
-    tree * root=NULL;
-    root=t.insert(root,1);
-    root=t.insert(root,20);
-    t.inorder(root);
-    root=t.insert(root,2);
-    root=t.insert(root,6);
-    root=t.insert(root,7);
-    t.inorder(root);
-    root=t.insert(root,3);
-    root=t.insert(root,5);
-    t.inorder(root);
-    root=t.insert(root,9);
-    t.inorder(root);
- //  cout << "Enter nodes to insert ";
+// tree * tree::insert(tree *root, int value)
+// {
+//     if (!root)
+//         return new tree(value);
+
+//     if (value > root->data)
+//         root->right = insert(root->right, value);
+//     else
+//     {
+//         root->left = insert(root->left, value);
+//     }
+//     return root;
+// }
+//  void tree::inorder(tree *root)
+// {
+//     cout<<"\n";
+//     if (root == 0)
+//         return;
+//     else
+//     {
+
+//         inorder(root->left);
+//         cout << root->data << " ";
+//         inorder(root->right);
+//     }
+// }
+// // void deletion()
+// // {
+// // }
+// int main()
+// {
+
+//     tree t;
+//     tree * root=NULL;
+//     root=t.insert(root,1);
+//     root=t.insert(root,20);
+//     t.inorder(root);
+//     root=t.insert(root,2);
+//     root=t.insert(root,6);
+//     root=t.insert(root,7);
+//     t.inorder(root);
+//     root=t.insert(root,3);
+//     root=t.insert(root,5);
+//     t.inorder(root);
+//     root=t.insert(root,9);
+//     t.inorder(root);
+//  //  cout << "Enter nodes to insert ";
+// }
+#include <stdio.h>
+#include <stdlib.h>
+struct tree
+{
+    struct tree *lchild;
+    char data[10];
+    struct tree *rchild;
+};
+typedef struct tree node;
+int ctr;
+node *tree[100];
+node *getnode()
+{
+    node *temp;
+    temp = (node *)malloc(sizeof(node));
+    printf("\n Enter Data: ");
+    scanf("%s", temp->data);
+    temp->lchild = NULL;
+    temp->rchild = NULL;
+    return temp;
+}
+void create_fbinarytree()
+{
+    int j, i = 0;
+    printf("\n How many nodes you want: ");
+    scanf("%d", &ctr);
+    tree[0] = getnode();
+    j = ctr;
+    j--;
+    do
+    {
+        if (j > 0) /* left child */
+        {
+            tree[i * 2 + 1] = getnode();
+            tree[i]->lchild = tree[i * 2 + 1];
+            j--;
+        }
+        if (j > 0) /* right child */
+        {
+            tree[i * 2 + 2] = getnode();
+            j--;
+            tree[i]->rchild = tree[i * 2 + 2];
+        }
+        i++;
+    } while (j > 0);
+}
+void inorder(node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->lchild);
+        printf("%3s", root->data);
+        inorder(root->rchild);
+    }
+}
+void preorder(node *root)
+{
+    if (root != NULL)
+    {
+        printf("%3s", root->data);
+        preorder(root->lchild);
+        preorder(root->rchild);
+    }
+}
+void postorder(node *root)
+{
+    if (root != NULL)
+    {
+        postorder(root->lchild);
+        postorder(root->rchild);
+        printf("%3s", root->data);
+    }
+}
+void levelorder()
+{
+    int j;
+    for (j = 0; j < ctr; j++)
+    {
+        if (tree[j] != NULL)
+            printf("%3s", tree[j]->data);
+    }
+}
+void print_leaf(node *root)
+{
+    if (root != NULL)
+    {
+        if (root->lchild == NULL && root->rchild == NULL)
+            printf("%3s ", root->data);
+        print_leaf(root->lchild);
+        print_leaf(root->rchild);
+    }
+}
+// void height(node *root)
+// {
+// if(root == NULL)
+//  {
+//  return 0;
+//  }
+// //Lecture Notes 15 1 Dept. of Information Technology
+// if(root->lchild == NULL && root->rchild == NULL)
+//  return 0;
+//  else
+// {
+//   int ans;
+//   height(root->lchild) > height(root->rchild) ? cout<< height(root->lchild) : cout<< (height(root->rchild));
+
+//   return (ans+1);}
+// }
+void main()
+{
+    int i;
+    create_fbinarytree();
+    printf("\n Inorder Traversal: ");
+    inorder(tree[0]);
+    printf("\n Preorder Traversal: ");
+    preorder(tree[0]);
+    printf("\n Postorder Traversal: ");
+    postorder(tree[0]);
+    printf("\n Level Order Traversal: ");
+    levelorder();
+    printf("\n Leaf Nodes: ");
+    print_leaf(tree[0]);
+    //printf("\n Height of Tree: %d ", height(tree[0]));
+    //height(tree[0]);
 }
--- Trees/Binary_Tree_implementation.cpp
+++ Trees/Binary_Tree_implementation.cpp
@@ -35,6 +35,70 @@ void preorder(node *root)
         preorder(root->right);
     }
 }
+node* deleteNode(node* root, int k)
+{
+    // Base case
+    if (root == NULL)
+        return root;
+ 
+    // Recursive calls for ancestors of
+    // node to be deleted
+    if (root->key > k) {
+        root->left = deleteNode(root->left, k);
+        return root;
+    }
+    else if (root->key < k) {
+        root->right = deleteNode(root->right, k);
+        return root;
+    }
+ 
+    // We reach here when root is the node
+    // to be deleted.
+ 
+    // If one of the children is empty
+    if (root->left == NULL) {
+        Node* temp = root->right;
+        delete root;
+        return temp;
+    }
+    else if (root->right == NULL) {
+        Node* temp = root->left;
+        delete root;
+        return temp;
+    }
+ 
+    // If both children exist
+    else {
+ 
+        Node* succParent = root;
+ 
+        // Find successor
+        Node* succ = root->right;
+        while (succ->left != NULL) {
+            succParent = succ;
+            succ = succ->left;
+        }
+ 
+        // Delete successor.  Since successor
+        // is always left child of its parent
+        // we can safely make successor's right
+        // right child as left of its parent.
+        // If there is no succ, then assign
+        // succ->right to succParent->right
+        if (succParent != root)
+            succParent->left = succ->right;
+        else
+            succParent->right = succ->right;
+ 
+        // Copy Successor Data to root
+        root->key = succ->key;
+ 
+        // Delete Successor and return root
+        delete succ;
+        return root;
+    }
+}
+ 
 void inorder(node *root)
 {
     if (root == 0)
