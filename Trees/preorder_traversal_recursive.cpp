236ced1a951fd59fd506c6ea454f169dc6f046a2
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2021-08-23 09:49:13 +0530

    Create preorder_traversal_recursive.cpp

---
+++ Trees/preorder_traversal_recursive.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void find(TreeNode* root,vector<int>& v)
+    {
+        if(root==NULL)
+            return;
+        v.push_back(root->val);
+        find(root->left,v);
+        find(root->right,v);
+        
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>v;
+        find(root,v);
+        return v;
+        
+    }
+};
