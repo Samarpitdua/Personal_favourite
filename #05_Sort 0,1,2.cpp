9474d7429ebeaf493ef6b2d5ee0f2cd37c073ab8
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2021-11-16 20:32:42 +0530

    Create #05_Sort 0,1,2.cpp

---
+++ #05_Sort 0,1,2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    //We will use a 3 pointer approach here
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int low=0,mid=0,high=n-1;
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                if(mid>low)
+                {
+                    swap(nums[mid],nums[low]);
+                    low++;
+                }
+                else
+                    mid++;
+            }
+            else if(nums[mid]==1)
+                mid++;
+            else
+            {
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+        
+    }
+};
